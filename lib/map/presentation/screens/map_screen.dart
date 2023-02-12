62a0f470729b888f178d2c80bc58bbc7842d5790
dmitry-lyovochkin <80591456+dmitry-lyovochkin@users.noreply.github.com>
2023-02-12 18:33:01 +0300

    Refactoring map_screen

--- lib/map/presentation/screens/map_screen.dart
+++ lib/map/presentation/screens/map_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:yandex_map_example/map/domain/app_latitude_longitude.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
@@ -12,7 +14,12 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  YandexMapController? _mapController;
+  final Completer<YandexMapController> _mapController =
+      Completer<YandexMapController>();
+
+  Future<YandexMapController> get mapController {
+    return _mapController.future;
+  }
 
   @override
   void initState() {
@@ -28,7 +35,7 @@ class _MapScreenState extends State<MapScreen> {
       ),
       body: YandexMap(
         onMapCreated: (controller) {
-          _mapController = controller;
+          _mapController.complete(controller);
           // show(context);
         },
       ),
@@ -40,45 +47,39 @@ class _MapScreenState extends State<MapScreen> {
     if (!await LocationService().checkPermission()) {
       await LocationService().requestPermission();
     }
-      await _fetchCurrentLocation();
+    await _fetchCurrentLocation();
   }
 
   /// Получение текущей геопозиции пользователя
   Future<void> _fetchCurrentLocation() async {
-     LocationService().getCurrentLocation().then((position) {
-      try {
-        _moveToCurrentPosition(AppLatLong(
-          lat: position.lat,
-          long: position.long,
-        ));
-      } on Exception catch (_) {
-        _moveToCurrentPosition(AppLatLong(
-          lat: MoscowLocation().latMoscow,
-          long: MoscowLocation().longMoscow,
-        ));
-      }
-    });
+    AppLatLong location;
+    try {
+      location = await LocationService().getCurrentLocation();
+    } catch (_) {
+      final defLocation = MoscowLocation();
+      location = AppLatLong(
+        lat: defLocation.latMoscow,
+        long: defLocation.longMoscow,
+      );
+    }
+    _moveToCurrentLocation(location);
   }
 
   /// Метод для показа текущей позиции
-  Future<void> _moveToCurrentPosition(
+  Future<void> _moveToCurrentLocation(
     AppLatLong appLatLong,
   ) async {
-    // Проверка контроллера на работоспособность
-    if (_mapController != null) {
-      await _mapController!.moveCamera(
-        animation:
-            const MapAnimation(type: MapAnimationType.linear, duration: 1),
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: Point(
-              latitude: appLatLong.lat,
-              longitude: appLatLong.long,
-            ),
-            zoom: 12,
+    (await mapController).moveCamera(
+      animation: const MapAnimation(type: MapAnimationType.linear, duration: 1),
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: Point(
+            latitude: appLatLong.lat,
+            longitude: appLatLong.long,
           ),
+          zoom: 12,
         ),
-      );
-    }
+      ),
+    );
   }
 }
